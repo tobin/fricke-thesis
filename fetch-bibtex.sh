e6a4e3bb720250061e584fbf482405513e245cf9
Tobin Fricke <tobin@pas.rochester.edu>
2011-09-28 18:41:54 -0400

    some scripts

--- fetch-bibtex.sh
+++ fetch-bibtex.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-wget --output-document=tobin.bib "http://www.citeulike.org/bibtex/user/tobin?key_type=4"
+wget --output-document=tobin.bib "http://www.citeulike.org/bibtex/user/tobin?key_type=4&incl_amazon=0&clean_urls=0"
 ./remove-doi-urls.sh
+bibtex dissertation
+
