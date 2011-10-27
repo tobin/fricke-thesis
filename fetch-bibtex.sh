8f9d0b4b050784fb06764368a9f40fbe7088a7aa
Tobin Fricke <tobin@pas.rochester.edu>
2011-10-26 20:19:41 -0500

    Move 'LIGO Cavity Lengths and Mirror Reflectivities' section to its own appendix

--- fetch-bibtex.sh
+++ fetch-bibtex.sh
@@ -2,4 +2,6 @@
 wget --output-document=tobin.bib "http://www.citeulike.org/bibtex/user/tobin?key_type=4&incl_amazon=0&clean_urls=0"
 ./remove-doi-urls.sh
 bibtex dissertation
+# Remove non-public DCC links since they bother everyone so much
+perl -i -ne 'print if !(/private/)' tobin.bib
 
